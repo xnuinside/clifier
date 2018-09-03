a06db24a46c1f1cffe1b229528b9fd0811f999f7
xnuinside <xnuinside@gmail.com>
2018-09-03 12:07:26 +0300

    add example and custom actions

--- src/clifier/clifier.py
+++ src/clifier/clifier.py
@@ -3,7 +3,7 @@ import yaml
 import logging
 import argparse
 
-logger = logging.getLogger("laziest")
+logger = logging.getLogger("clifier")
 
 
 class Clifier(object):
@@ -13,7 +13,8 @@ class Clifier(object):
         'nargs': 1
     }
 
-    def __init__(self, config_path, use_base_defaults=False, defaults=None):
+    def __init__(self, config_path, prog_version=None,
+                 use_base_defaults=False, defaults=None):
         """
 
         :param config_path:
@@ -24,6 +25,8 @@ class Clifier(object):
         self.config = self.read_config()
         self.defaults = defaults if defaults \
             else self.defaults if use_base_defaults else None
+        self.prog_version = prog_version if prog_version else self.config.get(
+            "parser", {}).get("version")
 
     def create_parser(self):
         """ """
@@ -43,9 +46,10 @@ class Clifier(object):
         return parser
 
     def apply_defaults(self, commands):
-        """ apply default settings to commands """
+        """ apply default settings to commands
+            not static, shadow "self" in eval
+        """
         for command in commands:
-            print(command)
             if 'action' in command and "()" in command['action']:
                 command['action'] = eval("self.{}".format(command['action']))
             if command['keys'][0].startswith('-'):
@@ -79,3 +83,49 @@ class Clifier(object):
     def add_actions(self, actions_list):
         for action in actions_list:
             self.__setattr__(action.__name__, action)
+
+    def show_version(self):
+        """ custom command line  action to show version """
+        class ShowVersionAction(argparse.Action):
+            def __init__(inner_self, nargs=0, **kw):
+                super().__init__(nargs=nargs, **kw)
+
+            def __call__(inner_self, parser, args, value, option_string=None):
+                print("{parser_name} version: {version}".format(
+                    parser_name=self.config.get(
+                        "parser", {}).get("prog"),
+                    version=self.prog_version))
+        return ShowVersionAction
+
+    def check_path_action(self):
+        """ custom command line action to check file exist """
+        class CheckPathAction(argparse.Action):
+            def __call__(self, parser, args, value, option_string=None):
+                if type(value) is list:
+                    value = value[0]
+                user_value = value
+                if option_string == 'None':
+                    if not os.path.isdir(value):
+                        _current_user = os.path.expanduser("~")
+                        if not value.startswith(_current_user) \
+                                and not value.startswith(os.getcwd()):
+                            if os.path.isdir(os.path.join(_current_user, value)):
+                                value = os.path.join(_current_user, value)
+                            elif os.path.isdir(os.path.join(os.getcwd(), value)):
+                                value = os.path.join(os.getcwd(), value)
+                            else:
+                                value = None
+                        else:
+                            value = None
+                elif option_string == '--template-name':
+                    if not os.path.isdir(value):
+                        if not os.path.isdir(os.path.join(args.target, value)):
+                            value = None
+                if not value:
+                    logger.error("Could not to find path %s. Please provide "
+                                 "correct path to %s option",
+                                 user_value, option_string)
+                    exit(1)
+                setattr(args, self.dest, value)
+
+        return CheckPathAction
